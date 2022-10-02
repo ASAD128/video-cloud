6101dd77cebf3defe21ea2027b9cf5a8903088de
Asad Hussain <iamasad128@gmail.com>
2022-10-03 03:01:17 +0800

    Clean up the project

--- app/models/video.rb
+++ app/models/video.rb
@@ -10,8 +10,8 @@ class Video < ApplicationRecord
   def file_format
     return unless file.attached?
     if file.blob.content_type.end_with?('video/mp4') || file.blob.content_type.end_with?('video/mov')
-      if file.blob.byte_size > 10.megabytes
-        errors.add(:file, 'size needs to be less than 10MB')
+      if file.blob.byte_size > 200.megabytes
+        errors.add(:file, 'size needs to be less than 200MB')
         file.purge
       end
     else
