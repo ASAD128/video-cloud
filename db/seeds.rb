15d4eecdf2dae5335ecee7203f01580853d18357
Asad Hussain <iamasad128@gmail.com>
2022-10-02 14:40:37 +0800

    Seed category data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts "Seeding..."
+
+Category.create(name: 'Exercise')
+Category.create(name: 'Education')
+Category.create(name: 'Recipe')
+
+puts "Seeding done."
