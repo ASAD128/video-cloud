7df92ec6e292a01fbc800567711a689508f14c2c
Asad Hussain <iamasad128@gmail.com>
2022-10-02 16:19:17 +0800

    Write unit tests for model

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -20,47 +20,18 @@ class VideosController < ApplicationController
   end
 
   # POST /videos or /videos.json
-=begin
   def create
     @video = Video.new(video_params)
-
     respond_to do |format|
       if @video.save
-        format.html { redirect_to video_url(@video), notice: "Video was successfully created." }
-        format.json { render :show, status: :created, location: @video }
+        generate_thumbnails
+        format.html { redirect_to controller: "videos", action: "index", format: "html", notice: 'Video was successfully created.' }
+        # format.json { render :show, status: :created, location: @video }
       else
         format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @video.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-=end
-
-  # POST /videos or /videos.json
-  def create
-    @video = Video.new(video_params)
-    respond_to do |format|
-      if @video.save
-        byebug
-        VideoProcessingService::GenerateThumbnail.new(video_id: @video.id).call
-        # thumb_nail = @video.file.preview(resize: "300X300").processed
-        # path = Rails.application.routes.url_helpers.rails_blob_path(@video.file.preview(resize: "300X300").image, only_path: true)
-        #file = @video.file.preview(resize: "300X300").image
-        # processed = ImageProcessing::MiniMagick.source(file).resize_to_limit(400, 400).convert("png").call
-        # processed = ImageProcessing::MiniMagick.source(thumb_nail.image).resize_to_limit(400, 400).convert("png").call
-        # ImageProcessing::MiniMagick .convert("png").resize_to_limit(400, 400).call(file)
-        # image = MiniMagick::Image.read(thumb_nail.image.download)
-
-        format.html { redirect_to controller: "videos", action: "index", format: "html", notice: 'Video was successfully created.' }
-        # format.json { render :show, status: :created, location: @article }
-      else
-        # format.html { render :new }
-        format.html { redirect_to controller: "videos", action: "index", format: "html", notice: "#{@video.errors}" }
-        # Rails.application.routes.url_helpers.rails_representation_url(object.video.preview(resize: "200x200").processed, only_path: true)
         # format.json { render json: @video.errors, status: :unprocessable_entity }
       end
     end
-    # render json: { status: '200' }
   end
 
   # PATCH/PUT /videos/1 or /videos/1.json
@@ -96,4 +67,8 @@ class VideosController < ApplicationController
     def video_params
       params.require(:video).permit(:title, :category_id, :file)
     end
+
+  def generate_thumbnails
+    VideoProcessingService::GenerateThumbnail.new(video_id: @video.id).call
+  end
 end
---
+++ spec/models/video_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+RSpec.describe Video, type: :model do
+  subject {
+    described_class.new(title: "Exercise Video")
+  }
+
+  it "is valid with valid attributes" do
+    expect(subject).to be_valid
+  end
+
+  it "is not valid without a title" do
+    subject.title = nil
+    expect(subject).to_not be_valid
+  end
+end
