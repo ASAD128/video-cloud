4232a37a738db861b5238870515664f99fe4b67c
Asad Hussain <iamasad128@gmail.com>
2022-10-03 10:01:24 +0800

    Fix .mov file content_type issue

--- app/models/video.rb
+++ app/models/video.rb
@@ -9,14 +9,15 @@ class Video < ApplicationRecord
   private
   def file_format
     return unless file.attached?
-    if file.blob.content_type.end_with?('video/mp4') || file.blob.content_type.end_with?('video/mov')
+    # .mov file content_type "video/quicktime"
+    if file.blob.content_type.end_with?('video/mp4') || file.blob.content_type.end_with?('video/quicktime')
       if file.blob.byte_size > 200.megabytes
         errors.add(:file, 'size needs to be less than 200MB')
         file.purge
       end
     else
       file.purge
-      errors.add(:file, 'needs to be an video of mp4 or mov format only')
+      errors.add(:file, 'needs to be an video of .mp4 or .mov format only')
     end
   end
 end
